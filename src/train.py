c1eb31781dcab0c4c7461cd6b58b9e55a09d3979
UwU-tao <hieungo1410@gmail.com>
2023-12-20 22:41:24 +0700

    update

--- src/train.py
+++ src/train.py
@@ -13,7 +13,7 @@ from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch import nn
 from torch.utils.tensorboard import SummaryWriter
 from torchmetrics.classification import MultilabelF1Score, MultilabelRecall, MultilabelPrecision
-import torchvision.models as models
+# import torchvision.models as models
 
 from src.metrics import *
 
