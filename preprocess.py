175f115d63060e31fbbfa71e63b4adb48b0c6b1c
UwU-tao <hieungo1410@gmail.com>
2023-12-20 22:47:58 +0700

    update

--- preprocess.py
+++ preprocess.py
@@ -63,6 +63,6 @@ for ftype in data.keys():
   data[ftype].drop(idx, inplace=True)
   data[ftype].reset_index(drop=True, inplace=True)
   
-data['train'].to_csv('data/dataset/train.dat', index=False)
-data['dev'].to_csv('data/dataset/dev.dat', index=False)
-data['test'].to_csv('data/dataset/test.dat', index=False)
+data['train'].to_csv('data/dataset/train.dat', index=False, header=False)
+data['dev'].to_csv('data/dataset/dev.dat', index=False, header=False)
+data['test'].to_csv('data/dataset/test.dat', index=False, header=False)
