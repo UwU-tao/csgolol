44a7ef2e1a13d2a7772bba3e48fc128dff0f8d2d
UwU-tao <hieungo1410@gmail.com>
2023-12-21 01:22:01 +0700

    update

--- main.py
+++ main.py
@@ -55,9 +55,9 @@ train_data = get_data(args, 'train')
 valid_data = get_data(args, 'dev')
 test_data = get_data(args, 'test')
 
-train_loader = DataLoader(train_data, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
-valid_loader = DataLoader(valid_data, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
-test_loader = DataLoader(test_data, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers)
+train_loader = DataLoader(train_data, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers, generator=torch.Generator(device='cuda'))
+valid_loader = DataLoader(valid_data, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers, generator=torch.Generator(device='cuda'))
+test_loader = DataLoader(test_data, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers, generator=torch.Generator(device='cuda'))
 print('Finish loading the data....')
 
 
