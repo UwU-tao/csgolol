f6dc88dda240dd7933fb0e9888c97297fe028ba8
UwU-tao <hieungo1410@gmail.com>
2023-12-24 16:24:13 +0700

    add

--- src/models.py
+++ src/models.py
@@ -155,15 +155,15 @@ class BasicModel(nn.Module):
         self.pool = nn.MaxPool2d(4, 4)
         self.conv2 = nn.Conv2d(16, 64, kernel_size=3, stride=1, padding=1)
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
-        # self.fc_cnn = nn.Linear(128 * 14 * 14, 128)
+        self.fc_cnn = nn.Linear(128 * 14 * 14, 128)
 
         self.embed = nn.Embedding(200, 30)
         self.lstm = nn.LSTM(input_size=30, hidden_size=128, num_layers=2, batch_first=True)
         # self.fc_lstm = nn.Linear(64, 128)
 
-        self.fc1 = nn.Linear(128 * 14 * 14 + 128, 3136)
-        self.fc2 = nn.Linear(3136, 196)
-        self.fc3 = nn.Linear(196, 18)
+        self.fc1 = nn.Linear(128, 64)
+        self.fc2 = nn.Linear(64, 18)
+        # self.fc3 = nn.Linear(196, 18)
 
         self.dropout = nn.Dropout(0.2)
         self.relu = nn.ReLU(inplace=True)
@@ -179,15 +179,15 @@ class BasicModel(nn.Module):
         images = torch.flatten(images, 1)
         # images = self.fc_cnn(images)
         
-        out = torch.cat((hidden[-1], images), dim=1)
-        out = self.dropout(out)
+        # out = torch.cat((hidden[-1], images), dim=1)
+        out = self.dropout(hidden[-1] * 0.3 + images * 0.7)
         out = self.fc1(out)
         out = self.relu(out)
         out = self.dropout(out)
         out = self.fc2(out)
-        out = self.relu(out)
-        out = self.dropout(out)
-        out = self.fc3(out)
+        # out = self.relu(out)
+        # out = self.dropout(out)
+        # out = self.fc3(out)
         return out
 
 class VGG_LSTMModel(nn.Module):
