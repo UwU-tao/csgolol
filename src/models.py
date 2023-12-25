b26ef2fc5a3226daf828bd0e6a28f47478fc181f
UwU-tao <hieungo1410@gmail.com>
2023-12-25 12:38:32 +0700

    add

--- src/models.py
+++ src/models.py
@@ -424,7 +424,6 @@ class RatingwVGGnBERT_wsModel(nn.Module):
         text = torch.mean(outs.last_hidden_state, dim=1)
         
         images = self.ext(images)
-        images = self.linear_ext(images)
         
         ratings = self.relu(self.rate_lin1(ratings))
         ratings = self.rate_bn1(ratings)
